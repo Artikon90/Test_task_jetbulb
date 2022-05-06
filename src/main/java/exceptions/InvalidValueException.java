966172677345db6f6a22a49df1abb9437ff2aee8
Artikon90 <artikon90@gmail.com>
2022-05-06 19:41:12 +0300

    Убрал оверрайд метода в исключении, кетч не юзается

--- src/main/java/exceptions/InvalidValueException.java
+++ src/main/java/exceptions/InvalidValueException.java
@@ -4,8 +4,4 @@ public class InvalidValueException extends RuntimeException {
     public InvalidValueException() {
         super("String must contain only valid roman numerals [I, V, X, L, C, D, M].");
     }
-    @Override
-    public void printStackTrace() {
-        System.out.println("String must contain only valid roman numerals [I, V, X, L, C, D, M].");
-    }
 }
