46495cdfb2c1a15a507ac2370e67de91a24caa71
artikon90 <artikon90@gmail.com>
2022-05-09 01:30:44 +0300

    Улучшил проверку на null

--- src/main/java/logics/NotationConverter.java
+++ src/main/java/logics/NotationConverter.java
@@ -5,9 +5,6 @@ import java.util.HashMap;
 
 public class NotationConverter {
     public int toArabic(String romanNotation) throws InvalidValueException {
-        if(romanNotation == null) {
-            throw new InvalidValueException();
-        }
         HashMap<Character, Integer> romanNumber = new HashMap<>();
         romanNumber.put('I', 1);
         romanNumber.put('V', 5);
@@ -16,12 +13,12 @@ public class NotationConverter {
         romanNumber.put('C', 100);
         romanNumber.put('D', 500);
         romanNumber.put('M', 1000);
-        //раскомментируй следующую строку, если требуется валидация без учета регистра
-        //romanNotation = romanNotation.toUpperCase();
-        char[] charArray = romanNotation.toCharArray();
-        if (romanNotation.isBlank()) {
+        char[] charArray;
+        if (romanNotation == null || romanNotation.isBlank()) {
             throw new InvalidValueException();
         } else {
+            //romanNotation = romanNotation.toUpperCase();
+            charArray = romanNotation.toCharArray();
             for (char i : charArray) {
                 if(!romanNumber.containsKey(i)) {
                     throw new InvalidValueException();
